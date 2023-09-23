c66e2a42e173e0f16c7441223cb18d965f1a72a1
Danger Loki <andremicael13@hotmail.com>
2023-09-22 23:47:29 -0300

    aws

--- js/index.js
+++ js/index.js
@@ -1,72 +1,30 @@
-let qrcode;
-let canvasElement;
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 
-window.gerarQRCode = function() {
-    if (!qrcode) {
-        let divElement = document.getElementById("qrcode");
-        qrcode = new QRCode(divElement, {
-            text: "https://www.youtube.com/",
-            width: 128,
-            height: 128
-        });
-        canvasElement = document.querySelector('#qrcode canvas');
-    } else {
-        qrcode.makeCode("https://www.youtube.com/");
-    }
-}
-
-window.compartilharQRCode = function() {
-    if (!qrcode) {
-        alert('Por favor, gere o QR Code primeiro.');
-        return;
-    }
-
-    let img = canvasElement.toDataURL("image/png");
-    let link = document.createElement('a');
-    link.download = 'qrcode.png';
-    link.href = img;
-    link.click();
-}
-
-window.abrirCamera = function() {
-  let video = document.getElementById('preview');
-  let statusElement = document.getElementById('status');
-
+window.compartilharQRCode = function () {
   if (!qrcode) {
-      alert('Por favor, gere o QR Code primeiro.');
-      return;
+    alert("Por favor, gere o QR Code primeiro.");
+    return;
   }
 
-  if (navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } })
-          .then(function (stream) {
-              video.srcObject = stream;
-              video.setAttribute("playsinline", true);
-              video.play();
-              setInterval(function(){
-                  let canvas = document.createElement('canvas');
-                  canvas.width = video.videoWidth;
-                  canvas.height = video.videoHeight;
-                  let ctx = canvas.getContext('2d');
-                  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-                  let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-                  let code = jsQR(imageData.data, imageData.width, imageData.height);
-                  if (code) {
-                      // Aqui você pode adicionar uma verificação para determinar se o código é válido
-                      if (code.data === 'dados esperados') {
-                          statusElement.textContent = 'APROVADO';
-                      } else {
-                          statusElement.textContent = 'Inválido';
-                      }
-                  } else {
-                      statusElement.textContent = 'Não lido';
-                  }
-              }, 500);
-          })
-          .catch(function (error) {
-              console.log("Something went wrong!");
-              statusElement.textContent = 'Erro';
-          });
-  }
-}
+  let img = canvasElement.toDataURL("image/png");
+  let base64Data = img.replace(/^data:image\/\w+;base64,/, "");
+  base64Data = Buffer.from(base64Data, 'base64');
 
+  const params = {
+    Bucket: 'NOME_DO_SEU_BUCKET',
+    Key: 'qrcode.png', 
+    Body: base64Data,
+    ContentEncoding: 'base64',
+    ContentType: 'image/png'
+  }
+
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log(`Erro ao enviar para o S3: ${err}`);
+    } else {
+      console.log(`Arquivo enviado com sucesso para o S3: ${data.Location}`);
+      // Aqui você pode adicionar o código para salvar os dados do QR Code no DynamoDB
+    }
+  });
+};
